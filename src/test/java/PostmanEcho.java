8ffa8fc25615d1051934f202204128ccf3cdee40
Aliaksandr Zuyeuski <negrustin87@gmail.com>
2022-08-11 04:53:29 +0200

    install

---
+++ src/test/java/PostmanEcho.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEcho {
+
+    @Test
+
+    public void postmanEchoPostTest(){
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Test Message")
+
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Test Message"))
+        ;
+    }
+}
